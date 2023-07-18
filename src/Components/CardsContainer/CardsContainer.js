655643ffed4592fb7a7c4ed56da9419dbe3ac135
Nicholas Leask <leask.nicholas@gmail.com>
2023-07-18 11:01:01 -0400

    feat: fetching tv maturity ratings

--- src/Components/CardsContainer/CardsContainer.js
+++ src/Components/CardsContainer/CardsContainer.js
@@ -1,32 +1,31 @@
 import { useEffect, useState } from 'react';
 import './CardsContainer.css'
 
-export default function CardsContainer({media}) {
+export default function CardsContainer({ media }) {
   const mediaBackdrop = `https://image.tmdb.org/t/p/original/${media.backdrop_path}`;
   // const maturityRatingUrl = `https://api.themoviedb.org/3/tv/${movie.id}/content_ratings?&api_key=fed5b7c4a0fea83e14866a8dd8cb6baa`
-  const maturityRatingUrl = `https://api.themoviedb.org/3/movie/${media.id}/release_dates?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa`
+  const maturityRatingUrl = media.media_type === 'movie' ?
+    `https://api.themoviedb.org/3/movie/${media.id}/release_dates?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa` :
+    `https://api.themoviedb.org/3/tv/1408/content_ratings?&api_key=fed5b7c4a0fea83e14866a8dd8cb6baa`
   const [maturityRating, setMaturityRating] = useState('');
 
   useEffect(() => {
-    if(media.media_type === 'movie'){
-      console.log(media.id)
-      fetch(maturityRatingUrl)
+    fetch(maturityRatingUrl)
       .then(req => req.json())
       .then(req => {
-        if(req.results){
-            for(let i = 0; i < req?.results?.length; i++){
-              if(req.results[i].iso_3166_1 === 'US') {
-                return req.results[i].release_dates[0].certification;
-              }
+        if (req.results) {
+          for (let i = 0; i < req?.results?.length; i++) {
+            if (req.results[i].iso_3166_1 === 'US') {
+              return media.media_type === 'movie' ? req.results[i].release_dates[0].certification :  req.results[i].rating;
             }
-            return req.results[0].release_dates[0].certification;
           }
-          console.log(req)
-          return 'N/A';
-        })
-        .then(req => setMaturityRating(req))
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+          return req.results[0].release_dates[0].certification;
+        }
+        console.log(req)
+        return 'N/A';
+      })
+      .then(req => setMaturityRating(req))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   return (
@@ -34,8 +33,8 @@ export default function CardsContainer({media}) {
       <img alt='movieBackdrop' src={mediaBackdrop} />
       <div className='cornerText topLeft'>{maturityRating}</div>
       <div className='cornerText topRight'>{Math.round(media.vote_average)}/10</div>
-      <div className='cornerText bottomLeft'>{media.release_date?.slice(0,4)}</div>
-      <div className='cornerText bottomRight'>{media.release_date?.slice(0,4)}</div>
+      <div className='cornerText bottomLeft'>{media.release_date?.slice(0, 4)}</div>
+      <div className='cornerText bottomRight'>{media.release_date?.slice(0, 4)}</div>
       <div className='cornerText title'>{media.title}</div>
       {/* <p className='hoverText'>{media.overview}</p> */}
     </div>
